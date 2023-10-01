9354155153427dd7b1672a7e2fd2a8c4d2a5bfce
penny271 <hirotaka.aoki27@gmail.com>
2023-10-02 08:01:49 +0900

    scssが動かない

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  # root "top#index"
+  # get 'top/index', to: 'top#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  namespace :admin do
+    root "top#index"
+  end
+
+  namespace :user do
+    root "top#index"
+  end
 end
